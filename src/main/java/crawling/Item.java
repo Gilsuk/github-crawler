d3d9108d0e8bcae1b2a552e264381169bf0a021e
Gilsuk <devgilsuk@gmail.com>
2020-04-02 21:24:14 +0900

    add Model Item to store parsed values

---
+++ src/main/java/crawling/Item.java
@@ -0,0 +1,51 @@
+package crawling;
+
+import java.util.List;
+
+public class Item {
+	
+	private int id;
+	private String title;
+	private int star;
+	private String description;
+	private List<String> lang;
+	private List<String> tag;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getStar() {
+		return star;
+	}
+	public void setStar(int star) {
+		this.star = star;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public List<String> getLang() {
+		return lang;
+	}
+	public void setLang(List<String> lang) {
+		this.lang = lang;
+	}
+	public List<String> getTag() {
+		return tag;
+	}
+	public void setTag(List<String> tag) {
+		this.tag = tag;
+	}
+
+}

c3e0520926e1c3500d79771e681ba9341fbfe31c
Gilsuk <devgilsuk@gmail.com>
2020-04-06 13:55:12 +0900

    refactor Main

--- src/main/java/exec/Exec.java
+++ src/main/java/exec/Exec.java
@@ -6,16 +6,18 @@ public class Exec {
 
 	public static void main(String[] args) throws Exception {
 		
-//		String path = "E:/Downloads/crawl_02.db";
-		String path = args[0];
+		String dbPath = "repos.db";
+		String url = "https://github.com/search?utf8=%E2%9C%93&q=%EB%94%A5%EB%9F%AC%EB%8B%9D&ref=simplesearch";
+		if (args.length == 2) {
+			dbPath = args[0];
+			url = args[1];
+		}
 
+		// Create scheme if not exist
 		Controller controller = new Controller();
-		controller.createDB(path);
-		
-		controller.crawl(
-			path,
-			"https://github.com/search?utf8=%E2%9C%93&q=%EB%94%A5%EB%9F%AC%EB%8B%9D&ref=simplesearch",
-			10,
-			6000);
+		controller.createDB(dbPath);
+
+		// crawl(dbpath, url to parse, trys if fail to connect, interval)
+		controller.crawl(dbPath, url, 10, 6000);
 	}
 }
